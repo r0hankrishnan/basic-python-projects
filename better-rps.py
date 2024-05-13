bc9d63c0cf9b5c5b09c5919e09b850d9f92ae330
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-13 15:52:28 -0400

    Create better-rps.py
    In the win condition, there is still a bug occurring. If you say "y" to play again it doesn't let you pick a new choice it just randomly generates a new com choice.

---
+++ better-rps.py
@@ -0,0 +1,42 @@
+# Remake the rock, paper, scissors game without having to explicitly define each potential outcome
+#Instead of listing each case, you can just define the tie and win scenarios and the rest have to be losses
+
+#Import random
+import random
+
+#Create game
+def better_rps():
+    play_again = " "
+
+    while play_again != "n":
+        user_choice = input("Pick rock(r), paper(p), or scissors(s): ").strip()
+        com_choice = random.choice(["r", "p", "s"])
+
+        if user_choice == com_choice:
+            print("It's a tie!")
+            play_again = input("Would you like to play again? (y/n) ")
+        
+        #If statements are boolean so don't need to explicitly define "true" after statement
+        if win(user_choice, com_choice):
+            print(f"Your choice: {user_choice}")
+            print(f"Computer choice: {com_choice}")
+            print(f"You win!")
+            play_again = input("Would you like to play again? (y/n) ")
+        
+        #Lose scenario (runs if win() returns false)
+        print(f"Your choice: {user_choice}")
+        print(f"Computer choice: {com_choice}")
+        print(f"You lose!")
+        play_again = input("Would you like to play again? (y/n) ")
+    
+    #Runs after player says they don't want to play again
+    print("Thanks for playing!")
+
+#Define win function
+def win(user, com):
+    if (user == "r" and com == "s") or (user == "p" and com == "r") or (user == "s" and com == "p"):
+        return True
+    
+    return False
+
+better_rps()
