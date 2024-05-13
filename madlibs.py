d609639193b38d343cc8bba4b9b95726398cb42a
r0hankrishnan <rohan.krishnan.2020@gmail.com>
2024-05-13 14:59:06 -0400

    Create madlibs.py
    Simple madlibs program

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#Create a mad libs program
+sport = input("Pick a sport: ").strip()
+adj1 = input("Pick an adjective:").strip()
+adj2 = input("Pick an adjective:").strip()
+verb = input("Pick a verb: ").strip()
+
+
+#Define output
+print(f"I love playing {sport}. It is so fun and I like it alot because I am {adj1} and {adj2}. Ok, gotta go and {verb}!")
